a47ea0465298f173d309afefc76672d06f576a56
Naveen <Naveen@lauchyard.com>
2016-03-07 18:20:26 +0530

    rendering template

--- src/gitrepos/handler.clj
+++ src/gitrepos/handler.clj
@@ -1,10 +1,21 @@
 (ns gitrepos.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [clostache.parser :as clostache]))
+
+(defn read-template [template-name]
+  (slurp (clojure.java.io/resource
+    (str "templates/" template-name ".mustache"))))
+
+(defn render-template [template-file params]
+   (clostache/render (read-template template-file) params))
+
+(defn index []
+  (render-template "index" {:greeting "Bonjour"}))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] (index))
   (route/not-found "Not Found"))
 
 (def app
